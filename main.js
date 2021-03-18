f29f19e9ab54fbd3273a571e05c26ba47d934f7d
JoshuaJHaines <joshjhaines3@gmail.com>
2021-03-18 13:16:58 -0400

    called the function several times.

--- main.js
+++ main.js
@@ -9,9 +9,15 @@ const firstUl = unorderedListItems[0]
 firstUl.style.textDecoration = "line-through"
 
 //write a function to change images?
-
+// takes in two strings. the ID of an item and the URL you want placed in the src section
 function addURLtoImage(idStr, URLstr){
-    const idItem = document.querySelector(`#${idStr}`);
-    idItem.src = `\"${URLstr}\"`
-
+    //we want to reference a const that can dynamically take in the idStr
+    const idItem = document.querySelector(`${idStr}`);
+    //now take that const and edit the src with the urlStr
+    idItem.src = `${URLstr}`
 }
+
+addURLtoImage("#image-1", "testimg.jpeg")
+addURLtoImage("#image-2", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.1oIP_8j9_4BHAKFcY86rFgHaHa%26pid%3DApi&f=1")
+addURLtoImage("#image-3", "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.fyH0MSX841r0jfao1YoYDQHaHa%26pid%3DApi&f=1")
+
